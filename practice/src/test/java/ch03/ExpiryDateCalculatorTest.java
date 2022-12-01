1a04d3d529490a7b00421db8fb2952b83734f198
donggi-lee-bit <devdonggilee@gmail.com>
2022-12-01 14:41:59 +0900

    feat: 이만원 이상 납부시 비례하여 만료일 계산하는 기능 추가
    - 예외 케이스 처리
      - firstBillingDate와 candidateExp의 일자가 다를 때 candidateExp의 일자를 firstBillingDate의 일자로 바꾸려고 하는데 4월 31일과 같이 존재하지 않는 달력이면,
    
      YearMonth 클래스의 lengthOfMonth() 메서드를 이용하여 달의 일자를 비교하여 그 달의 마지막 날을 반환할 수 있도록 구현

--- practice/src/test/java/ch03/ExpiryDateCalculatorTest.java
+++ practice/src/test/java/ch03/ExpiryDateCalculatorTest.java
@@ -68,6 +68,62 @@ class ExpiryDateCalculatorTest {
         assertExpiryDate(payData3, LocalDate.of(2019, 7, 31));
     }
 
+    @Test
+    void 이만원_이상_납부하면_비례하여_만료일_계산() {
+        assertExpiryDate(
+            PayData.builder()
+                .billingDate(LocalDate.of(2019, 3, 1))
+                .payAmount(20_000)
+                .build(),
+            LocalDate.of(2019, 5, 1)
+        );
+
+        assertExpiryDate(
+            PayData.builder()
+                .billingDate(LocalDate.of(2019, 3, 1))
+                .payAmount(30_000)
+                .build(),
+            LocalDate.of(2019, 6, 1)
+        );
+
+        assertExpiryDate(
+            PayData.builder()
+                .billingDate(LocalDate.of(2019, 3, 1))
+                .payAmount(50_000)
+                .build(),
+            LocalDate.of(2019, 8, 1)
+        );
+
+        assertExpiryDate(
+            PayData.builder()
+                .billingDate(LocalDate.of(2019, 3, 1))
+                .payAmount(70_000)
+                .build(),
+            LocalDate.of(2019, 10, 1)
+        );
+    }
+
+    @Test
+    void 첫_납부일과_만료일_일자가_다를_때_이만원_이상_납부() {
+        assertExpiryDate(
+            PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 1, 31))
+                .billingDate(LocalDate.of(2019, 2, 28))
+                .payAmount(20_000)
+                .build(),
+            LocalDate.of(2019, 4, 30)
+        );
+
+        assertExpiryDate(
+            PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 3, 31))
+                .billingDate(LocalDate.of(2019, 4, 30))
+                .payAmount(30_000)
+                .build(),
+            LocalDate.of(2019, 7, 31)
+        );
+    }
+
     private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator expiryDateCalculator = new ExpiryDateCalculator();
         LocalDate realExpiryDate = expiryDateCalculator.calculateExpiryDate(payData);
