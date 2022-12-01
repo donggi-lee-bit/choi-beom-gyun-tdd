6961a690b07a84baf3f599a53e199b3c15b6b7d5
donggi-lee-bit <devdonggilee@gmail.com>
2022-12-01 11:18:30 +0900

    refactor: 메서드를 이용하여 테스트 코드의 중복 제거

--- practice/src/test/java/ch03/ExpiryDateCalculatorTest.java
+++ practice/src/test/java/ch03/ExpiryDateCalculatorTest.java
@@ -1,28 +1,23 @@
 package ch03;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ExpiryDateCalculatorTest {
 
     @Test
     void 만원_납부하면_한달_뒤_만료일이_됨() {
-        LocalDate billingDate = LocalDate.of(2022, 11, 1);
-        int payAmount = 10_000;
+        assertExpiryDate(LocalDate.of(2022, 11, 1), 10_000, LocalDate.of(2022, 12, 1));
+        assertExpiryDate(LocalDate.of(2022, 6, 1), 10_000, LocalDate.of(2022, 7, 1));
 
+    }
+
+    private void assertExpiryDate(LocalDate billingDate, int payAmount,
+        LocalDate expectedExpiryDate) {
         ExpiryDateCalculator expiryDateCalculator = new ExpiryDateCalculator();
-        LocalDate expiryDate = expiryDateCalculator.calculateExpiryDate(billingDate, payAmount);
-
-        LocalDate billingDate2 = LocalDate.of(2022, 5, 1);
-        int payAmount2 = 10_000;
-
-        LocalDate expiryDate2 = expiryDateCalculator.calculateExpiryDate(billingDate2, payAmount2);
-
-        assertThat(LocalDate.of(2022, 12, 1)).isEqualTo(expiryDate);
-        assertThat(LocalDate.of(2022, 6, 1)).isEqualTo(expiryDate2);
+        LocalDate realExpiryDate = expiryDateCalculator.calculateExpiryDate(billingDate, payAmount);
+        assertThat(realExpiryDate).isEqualTo(expectedExpiryDate);
     }
 }
